b6233d6305dbe139a5549c8f47190dfe26caea58
robharting <robharting@hotmail.com>
2019-05-05 15:53:56 +0200

    added sinon stub and happy and unhappy tests

--- test.js
+++ test.js
@@ -1,8 +1,23 @@
 const expect = require('chai').expect;
+const sinon  = require('sinon');
+const request  = require('request');
 
 const index = require('./index.js');
 
-describe('getTODOById', () => {
+describe('todo exists: getTODOById', () => {
+    before(() => {
+        sinon
+            .stub(request, 'get')
+            .yields(null, null, JSON.stringify( {
+                id: 1,
+                title: 'todo by rob title',
+                completed: false} ));
+    });
+
+    after(() => {
+        request.get.restore();
+    });
+
     it('todo has id of 1', (done) =>  {
         index.getTODOById(1)
         .then((todo) => {
@@ -13,4 +28,40 @@ describe('getTODOById', () => {
             done(err)
             })
     });
+
+    it('todo has title and completed', (done) =>  {
+        index.getTODOById(1)
+            .then((todo) => {
+                expect(todo).to.have.property('title');
+                expect(todo).to.have.property('completed');
+                done();
+            })
+            .catch((err) => {
+                done(err)
+            })
+    });
+});
+
+describe('todo does not exist: getTODOById', () => {
+    before(() => {
+        sinon
+            .stub(request, 'get')
+            .yields(null, null, JSON.stringify( {} ));
+    });
+
+    after(() => {
+        request.get.restore();
+    });
+
+    it('todo when this does not exists', (done) =>  {
+        index.getTODOById('zzz')
+            .then((todo) => {
+                expect(todo.id).to.equal(undefined);
+                done();
+            })
+            .catch((err) => {
+                done(err)
+            })
+    });
+
 });
